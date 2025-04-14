080e94f896f78bc0aac7ae19baf208cb2f3604e3
FerMeza <30847755+FerMeza@users.noreply.github.com>
2025-04-13 19:22:08 -0600

    feat: cell class

--- graphics.py
+++ graphics.py
@@ -1,5 +1,66 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Cell():
+    def __init__(self,
+                 top_left: Point,
+                 bottom_right: Point, 
+                 has_left_wall: bool = True,
+                 has_right_wall: bool = True, 
+                 has_top_wall: bool = True, 
+                 has_bottom_wall: bool = True,
+                 ):
+        self.has_left_wall = has_left_wall
+        self.has_right_wall = has_right_wall
+        self.has_top_wall = has_top_wall
+        self.has_bottom_wall = has_bottom_wall
+        self._x1 = top_left.x
+        self._y1 = top_left.y
+        self._x2 = bottom_right.x
+        self._y2 = bottom_right.y
+    
+    def draw(self, canvas: Canvas, fill_color="black"):
+        if self.has_top_wall:
+            canvas.create_line(
+                self._x1,
+                self._y1,
+                self._x2,
+                self._y1,
+                fill=fill_color,
+                width=2
+            )
+        if self.has_left_wall:
+            canvas.create_line(
+                self._x1,
+                self._y1,
+                self._x1,
+                self._y2,
+                fill=fill_color,
+                width=2
+            )
+        if self.has_bottom_wall:
+            canvas.create_line(
+                self._x1,
+                self._y2,
+                self._x2,
+                self._y2,
+                fill=fill_color,
+                width=2
+            )
+        if self.has_right_wall:
+            canvas.create_line(
+                self._x2,
+                self._y1,
+                self._x2,
+                self._y2,
+                fill=fill_color,
+                width=2
+            )
+
 class Window():
     def __init__(self, width, height, title):
         self.__root = Tk()
@@ -25,11 +86,8 @@ class Window():
 
     def draw_line(self, line, fill_color="black"):
         line.draw(self.__canvas, fill_color)
-
-class Point():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def draw_cell(self, cell: Cell, fill_color: str = "black"):
+        cell.draw(self.__canvas, fill_color)
 
 class Line():
     def __init__(self, point1, point2):
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from graphics import Line, Point, Window
+from graphics import Cell, Point, Window
 
 def main():
     win = Window(800, 600, "Maze Solver")
-    l = Line(Point(50, 50), Point(400, 400))
-    win.draw_line(l, "black")
+    c = Cell(Point(50, 50), Point(200, 200))
+    c2 = Cell(Point(150, 100), Point(300, 200), True, True, False, False)
+    win.draw_cell(c, "black")
+    win.draw_cell(c2, "black")
     win.wait_for_close()
 
 if __name__ == "__main__":
